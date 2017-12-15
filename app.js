4b6793431639f1b8b943ae6a080abfa1a13a9ef8
nguyenhoamy1602 <nguyenhoamy1602@gmail.com>
2017-12-15 21:01:11 +1100

    Add 'Note Delete'

--- app.js
+++ app.js
@@ -114,6 +114,32 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
             note.title, note.text);
     }])
 
+.matches('Note.Delete', [(session, args, next) => {
+        if (noteCount(session.userData.notes) > 0) {
+            // Resolve and store any Note.Title entity passed from LUIS.
+            var title;
+            var intent = args.intent;
+            var entity = builder.EntityRecognizer.findEntity(intent.entities, 'Note.Title');
+            if (entity) {
+                // Verify that the title is in our set of notes.
+                title = builder.EntityRecognizer.findBestMatch(session.userData.notes, entity.entity);
+            }
+
+            // Prompt for note name
+            if (!title) {
+                builder.Prompts.choice(session, 'Which note would you like to delete?', session.userData.notes);
+            } else {
+                next({ response: title });
+            }
+        } else {
+            session.endDialog("No notes to delete.");
+        }
+    },
+    (session, results) => {
+        delete session.userData.notes[results.response.entity];        
+        session.endDialog("Deleted the '%s' note.", results.response.entity);
+    }])
+
 /*
 .matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
 */
