f51f5fef83d98b2997b38d5bfd4a22aad65892c4
nguyenhoamy1602 <nguyenhoamy1602@gmail.com>
2017-12-15 20:59:15 +1100

    Add 'Note Create'

--- app.js
+++ app.js
@@ -66,6 +66,54 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
    }
 })
 
+.matches('Note.Create', [(session, args, next) => {
+        // Resolve and store any Note.Title entity passed from LUIS.
+        var intent = args.intent;
+        var title = builder.EntityRecognizer.findEntity(intent.entities, 'Note.Title');
+
+        var note = session.dialogData.note = {
+          title: title ? title.entity : null,
+        };
+
+        // Prompt for title
+        if (!note.title) {
+            builder.Prompts.text(session, 'What would you like to call your note?');
+        } else {
+            next();
+        }
+    },
+    (session, results, next) => {
+        var note = session.dialogData.note;
+        if (results.response) {
+            note.title = results.response;
+        }
+
+        // Prompt for the text of the note
+        if (!note.text) {
+            builder.Prompts.text(session, 'What would you like to say in your note?');
+        } else {
+            next();
+        }
+    },
+    (session, results) => {
+        var note = session.dialogData.note;
+        if (results.response) {
+            note.text = results.response;
+        }
+
+        // If the object for storing notes in session.userData doesn't exist yet, initialize it
+        if (!session.userData.notes) {
+            session.userData.notes = {};
+            console.log("initializing session.userData.notes in CreateNote dialog");
+        }
+        // Save notes in the notes object
+        session.userData.notes[note.title] = note;
+
+        // Send confirmation to user
+        session.endDialog('Creating note named "%s" with text "%s"',
+            note.title, note.text);
+    }])
+
 /*
 .matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
 */
